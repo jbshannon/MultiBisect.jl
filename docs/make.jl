3d6974367c0136d626c7b6cbd1bc8c824fa43cca
Jack Shannon <j.b.shannon@lse.ac.uk>
2023-10-14 19:54:17 +0100

    fixed doc build

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter
 
 DocTestSetup = quote
     using MultiBisect
-    using MultiBisect: forwardinds, expandzeros, edgetuple, edgebounds, edgedim, domainindex, revert
+    using MultiBisect: forwardinds, expandzeros, edgetuple, edgebounds, edgedim, domainindex, revert, findhypercubes, corners
     import MultiBisect.Roots as Roots
 end
 DocMeta.setdocmeta!(MultiBisect, :DocTestSetup, DocTestSetup; recursive=true)
